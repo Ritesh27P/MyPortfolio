2570597d0424c4488ef4e1c9b84f5e0eab919114
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-07-22 00:02:56 +0530

    Minor updates

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 Bootstrap(app)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/')
+def redirect_home():
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
